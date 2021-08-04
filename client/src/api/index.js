75756aa0df30f319850558468427828b7382b632
jxuan101 <35944722+jxuan101@users.noreply.github.com>
2021-08-04 10:22:16 -0400

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: process.env.BASE_URI || 'http://localhost:5000/api',
+    baseURL: process.env.BASE_URI,
 })
 
 export const getTeamsByMap = map_name => api.get(`/maps/${map_name}`);
