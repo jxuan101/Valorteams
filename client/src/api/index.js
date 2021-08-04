4f4e0fbe46041a285e8030c7a55d2b0b857c00fd
jxuan101 <35944722+jxuan101@users.noreply.github.com>
2021-08-04 10:14:14 -0400

    update api route to env

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: process.env.BASE_URI || 'http://localhost:5000/api',
 })
 
 export const getTeamsByMap = map_name => api.get(`/maps/${map_name}`);
